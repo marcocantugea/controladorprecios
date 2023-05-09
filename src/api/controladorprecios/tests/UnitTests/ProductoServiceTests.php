ae47f2c183b1d5af0363999110dfd0f11fb0362e
Marco A. Cantu Gea <marco.cantu.g@gmail.com>
2023-05-09 11:19:27 -0600

    add unit test

--- src/api/controladorprecios/tests/UnitTests/ProductoServiceTests.php
+++ src/api/controladorprecios/tests/UnitTests/ProductoServiceTests.php
@@ -26,14 +26,14 @@ class ProductoServiceTests extends TestCase
        
        $this->productosRepositoryMock=$this->getMockBuilder(ProductosRepository::class)
                                                 ->setConstructorArgs(array($this->db))
-                                                ->onlyMethods(['add','getById'])
+                                                ->onlyMethods(['add','getById','getProductos'])
                                                 ->getMock();
 
        $this->productoMapperMock=$this->getMockBuilder(ProductoMapper::class)
                                         ->onlyMethods(['map','reverse'])
                                         ->getMock();
 
-       $this->productoService= new ProductoService($this->db,$this->productosRepositoryMock,$this->productoMapperMock);
+       $this->productoService= new ProductoService($this->productosRepositoryMock,$this->productoMapperMock);
     }
 
     public function test_ShouldAddProducto_Success(){
@@ -122,5 +122,189 @@ class ProductoServiceTests extends TestCase
         } catch (\Throwable $th) {
             $this->assertTrue(true);
         }
+    }
+
+    public function test_ShouldGetProductos_Success_NoFilters(){
+        
+        $this->productosRepositoryMock->method('getProductos')->willReturn($this->getMockProducts());
+
+        $productosFound=$this->productoService->getProductos([]);
+
+        $this->assertEquals(count($this->getMockProducts()),$productosFound['totalRecordsFound']);
+        $this->assertEquals(count($this->getMockProducts()),count($productosFound['data']));
+
+    }
+
+    public function test_ShouldGetProductos_Success_OneFilter(){
+        
+        $productoToReturn=array_filter($this->getMockProducts(), function($items){
+            return $items->codigo=='codigo1';
+        });
+
+        $returnItems=json_decode(json_encode($productoToReturn));
+
+        $this->productosRepositoryMock->method('getProductos')->willReturn($returnItems);
+
+        $productoDTOReturn=new ProductoDTO(
+            $productoToReturn[0]->nombre,
+            $productoToReturn[0]->descripcion,
+            $productoToReturn[0]->codigo,
+            $productoToReturn[0]->sku,
+            $productoToReturn[0]->upc,
+            $productoToReturn[0]->ean
+        );
+        $this->productoMapperMock->method('reverse')->willReturn($productoDTOReturn);
+
+
+        $filter=[
+            'codigo'=>['codigo1',null]
+        ];
+
+        $productosFound=$this->productoService->getProductos($filter);
+
+        $this->assertEquals(1,$productosFound['totalRecordsFound']);
+        $this->assertEquals(1,count($productosFound['data']));
+        $this->assertEquals('codigo1',$productosFound['data'][0]->codigo);
+    }
+
+    public function test_ShouldGetProductos_Success_SomeFilter(){
+
+        $productoToReturn=array_filter($this->getMockProducts(), function($items){
+            return $items->codigo=='codigo1' || $items->sku=='sku3';
+        });
+
+        $returnItems=json_decode(json_encode($productoToReturn));
+
+        $this->productosRepositoryMock->method('getProductos')->willReturn($returnItems);
+
+        $productoDTOReturn=new ProductoDTO(
+            $productoToReturn[0]->nombre,
+            $productoToReturn[0]->descripcion,
+            $productoToReturn[0]->codigo,
+            $productoToReturn[0]->sku,
+            $productoToReturn[0]->upc,
+            $productoToReturn[0]->ean
+        );
+
+        $this->productoMapperMock->method('reverse')->willReturn($productoDTOReturn);
+
+        $filter=[
+            'codigo'=>['codigo1',null],
+            'sku'=>['sku3',null]
+        ];
+
+        $productosFound=$this->productoService->getProductos($filter);
+
+        $this->assertEquals(2,$productosFound['totalRecordsFound']);
+        $this->assertEquals(2,count($productosFound['data']));
+    }
+
+    public function test_ShouldGetProductos_Success_WithOffset(){
+        
+        $productoToReturn=array_filter($this->getMockProducts(), function($items){
+            return $items->codigo=='codigo1' || $items->sku=='sku3';
+        });
+
+        $returnItems=json_decode(json_encode($productoToReturn));
+
+        $this->productosRepositoryMock->method('getProductos')->willReturn($returnItems);
+
+        $productoDTOReturn=new ProductoDTO(
+            $productoToReturn[0]->nombre,
+            $productoToReturn[0]->descripcion,
+            $productoToReturn[0]->codigo,
+            $productoToReturn[0]->sku,
+            $productoToReturn[0]->upc,
+            $productoToReturn[0]->ean
+        );
+
+        $this->productoMapperMock->method('reverse')->willReturn($productoDTOReturn);
+
+        $filter=[
+            'codigo'=>['codigo1',null],
+            'sku'=>['sku3',null]
+        ];
+
+        $productosFound=$this->productoService->getProductos($filter,offset:1);
+
+        $this->assertEquals(2,$productosFound['totalRecordsFound']);
+        $this->assertEquals(2,count($productosFound['data']));
+    }
+
+    public function test_ShouldGetProductos_Success_WithLimit(){
+
+        $productoToReturn=array_filter($this->getMockProducts(), function($items){
+            return $items->codigo=='codigo1' || $items->sku=='sku3';
+        });
+
+        $returnItems=json_decode(json_encode($productoToReturn));
+
+        $this->productosRepositoryMock->method('getProductos')->willReturn($returnItems);
+
+        $productoDTOReturn=new ProductoDTO(
+            $productoToReturn[0]->nombre,
+            $productoToReturn[0]->descripcion,
+            $productoToReturn[0]->codigo,
+            $productoToReturn[0]->sku,
+            $productoToReturn[0]->upc,
+            $productoToReturn[0]->ean
+        );
+
+        $this->productoMapperMock->method('reverse')->willReturn($productoDTOReturn);
+
+        $filter=[
+            'codigo'=>['codigo1',null],
+            'sku'=>['sku3',null]
+        ];
+
+        $productosFound=$this->productoService->getProductos($filter,limit:1);
+
+        $this->assertEquals(2,$productosFound['totalRecordsFound']);
+        $this->assertEquals(2,count($productosFound['data']));
+    }
+
+    private function getMockProducts(){
+        
+        $model1= new Producto(); 
+        $model1->nombre="productoPrueba1";
+        $model1->descripcion="descripcion prueba1";
+        $model1->codigo="codigo1";
+        $model1->sku="sku1";
+        $model1->upc="upc1";
+        $model1->ean="ean1";
+
+        $model2= new Producto(); 
+        $model2->nombre="productoPrueba2";
+        $model2->descripcion="descripcion prueba2";
+        $model2->codigo="codigo2";
+        $model2->sku="sku2";
+        $model2->upc="upc2";
+        $model2->ean="ean2";
+
+        $model3= new Producto(); 
+        $model3->nombre="productoPrueba3";
+        $model3->descripcion="descripcion prueba3";
+        $model3->codigo="codigo3";
+        $model3->sku="sku3";
+        $model3->upc="upc3";
+        $model3->ean="ean3";
+
+        $model4= new Producto(); 
+        $model4->nombre="productoPrueba4";
+        $model4->descripcion="descripcion prueba4";
+        $model4->codigo="codigo4";
+        $model4->sku="sku4";
+        $model4->upc="upc4";
+        $model4->ean="ean4";
+
+        $model5= new Producto(); 
+        $model5->nombre="productoPrueba5";
+        $model5->descripcion="descripcion prueba5";
+        $model5->codigo="codigo5";
+        $model5->sku="sku5";
+        $model5->upc="upc5";
+        $model5->ean="ean5";
+
+        return json_decode(json_encode([$model1,$model2,$model3,$model4,$model5]));
     }
 }
